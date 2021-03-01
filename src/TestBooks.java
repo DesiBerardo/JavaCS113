f14c69c6e14dfb0d8e683ab292485e0f443ce852
berar <db32@njit.edu>
2021-03-01 11:03:21 -0500

    finished testbooks

--- src/TestBooks.java
+++ src/TestBooks.java
@@ -6,50 +6,42 @@ public class TestBooks
     {
         Scanner in = new Scanner(System.in);
         String title = "";
-        int pages = 0;
-        int count = 1;
+        int pages = 1000000000;
+        int count = 0;
         int totalPages = 0;
         boolean loop = true;
-        System.out.println("Enter title of book (0 to exit): ");
-        if (in.next().equals("0"))
-            loop = false;
-        else
-            title = in.next();
 
-        System.out.println("Enter amount of pages (0 to exit): ");
-        if (in.nextInt() == 0)
-            loop = false;
-        else
-            pages = in.nextInt();
         Book book1 = new Book(pages, title);
         Book book2 = new Book(pages, title);
-        totalPages = pages;
 
         while (loop)
         {
             System.out.println("Enter title of book (0 to exit): ");
-            if (in.next().equals("0"))
+            title = in.next();
+            if (title.equals("0"))
                 break;
-            else
-                title = in.next();
 
-            System.out.println("Enter amount of pages (0 to exit): ");
-            if (in.nextInt() == 0)
-                break;
-            else
-                pages = in.nextInt();
+            System.out.println("Enter amount of pages: ");
+            pages = in.nextInt();
 
-            book2.setPages(pages);
-            book2.setTitle(title);
+            book1.setPages(pages);
+            book1.setTitle(title);
             count++;
             totalPages += pages;
 
-            if (book1.compareTo(book1, book2) == 1)
+            if (book1.compareTo(book1, book2) == -1)
             {
-                book2 = book1;
+                System.out.println("check");
+                book2.setTitle(book1.getTitle());
+                book2.setPages(book1.getPages());
             }
         }
-        System.out.println("The smallest book was: " + book2);
-        System.out.println("The average number of pages is: " + totalPages / count);
+        if (count == 0)
+            System.out.println("No books entered.");
+        else
+        {
+            System.out.println("The smallest book was: " + book2);
+            System.out.println("The average number of pages is: " + totalPages / (float) count);
+        }
     }
 }
