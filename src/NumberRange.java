850a8603ce3023e3f241b62496400c59b72a9a40
DesiBerardo <db32@njit.edu>
2021-03-25 21:04:02 -0400

    Hw 06, number range, testDice, testEvens.

---
+++ src/NumberRange.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class NumberRange
+{
+    public static void main(String[] args)
+    {
+        int[] range = new int[51];
+        int input;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many numbers would you like to enter?: ");
+        input = in.nextInt();
+
+        for (int i = 0; i < input; i++)
+        {
+            System.out.println("Please enter a number between 0 and 50:");
+            range[in.nextInt()]++;
+        }
+
+        for (int i = 0; i < range.length; i++)
+            System.out.println(i + ": " + range[i]);
+    }
+}
