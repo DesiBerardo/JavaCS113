aed726860b6b9e021b9a3b92915b7245b19a35a7
berar <db32@njit.edu>
2021-04-06 20:09:19 -0400

    4/4 fuck this homework

--- src/TestRecursion.java
+++ src/TestRecursion.java
@@ -21,9 +21,33 @@ public class TestRecursion
         }
     }
 
-    public static void pascalTriangle(int n)
+    public static void pascalTriangle(int n, int[] array, int i)
     {
+        if (n == 1)
+        {
+            System.out.println(n);
+        }
+        else
+        {
 
+        }
+    }
+
+    public static void print(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(pascal(i, j) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int pascal(int i, int j) {
+        if (j == 0 || j == i) {
+            return 1;
+        } else {
+            return pascal(i - 1, j - 1) + pascal(i - 1, j);
+        }
     }
 
     public static void main(String[] args)
@@ -31,6 +55,8 @@ public class TestRecursion
         int[] numArray = {1,2,3,4,5};
         printDigits(12345);
         System.out.println(sumArray(numArray, numArray.length - 1));
+
+        print(10);
 
     }
 }
