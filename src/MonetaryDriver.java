3937baad0ab7458bf1f8d61b0e51dc792fd61ad9
DesiBerardo <db32@njit.edu>
2021-04-19 16:28:08 -0400

    4/19 began hw 8

--- src/MonetaryDriver.java
+++ src/MonetaryDriver.java
@@ -2,10 +2,21 @@ public class MonetaryDriver
 {
     public static void main(String[] args)
     {
-        MonetaryCoin c = new MonetaryCoin((int)(Math.random() * 100));
+        MonetaryCoin[] c = new MonetaryCoin[35];//
+        int count = 0;
+        int total = 0;
 
-        c.flip();
+        for (int i = 0; i < 35; i++)
+        {
+            c[i] = new MonetaryCoin((int)(Math.random() * 100));
+            c[i].flip();
 
-        System.out.println(c);
+            if (c[i].isHeads())
+            {
+                count++;
+                total += c[i].getCents();
+            }
+        }
+        System.out.println( "The average cents with coin with head face is: " + total / count);
     }
 }
